b3fc6a23b4d50730583d7e11eccaee6093208862
M V K <krishna.mugala9@gmail.com>
2022-12-07 17:13:20 +0530

    connected all services to Eureka server and data is retrieved

--- springAPIGateWayService/src/main/java/com/niit/springAPIGateWayService/SpringApiGateWayServiceApplication.java
+++ springAPIGateWayService/src/main/java/com/niit/springAPIGateWayService/SpringApiGateWayServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringApiGateWayServiceApplication {
 
 	public static void main(String[] args) {
@@ -19,11 +21,11 @@ public class SpringApiGateWayServiceApplication {
 		return builder.routes()
 				.route(p -> p
 						.path("/api/v1/**")
-						.uri("http://UserAuthenticate:8080/")) // use the name of the application in the uri
+						.uri("lb://UserAuthenticate")) // use the name of the application in the uri
 
 				.route(p->p
 						.path("/api/v2/**")
-						.uri("http://customer-service:8086/"))
+						.uri("lb://customer-service"))
 				.build();
 	}
 
